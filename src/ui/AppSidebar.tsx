6608c207de80ce7a8cc6962b728f8a1cd8adf178
Ibrahimov Aydin <ibrahimovaydin33sh@gmail.com>
2025-07-10 14:56:35 +0400

    added: navigation and logo

--- src/ui/AppSidebar.tsx
+++ src/ui/AppSidebar.tsx
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import Logo from './Logo';
+import MainNav from './MainNav';
 
 const StyledAppSidebar = styled.aside`
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
   grid-row: 1 / -1;
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
@@ -8,5 +13,10 @@ const StyledAppSidebar = styled.aside`
 `;
 
 export default function AppSiderbar() {
-  return <StyledAppSidebar>Sidebar</StyledAppSidebar>;
+  return (
+    <StyledAppSidebar>
+      <Logo />
+      <MainNav />
+    </StyledAppSidebar>
+  );
 }
